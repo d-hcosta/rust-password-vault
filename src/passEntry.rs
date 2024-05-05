204caf4e078fff261e77c1819ad3d1a2c38bdad1
d-hcosta <d.hcosta@proton.me>
2024-05-05 16:44:17 -0300

    feat: Implementing user entry

--- src/passEntry.rs
+++ src/passEntry.rs
@@ -18,4 +18,35 @@ impl ServiceInfo {
       password,
     }
   }
+
+  pub fn from_json(json_string: &str) -> Result<Self, serde_json::Error> {
+    serde_json::from_str(json_string)
+  }
+
+  #[allow(dead_code)]
+  pub fn from_user_input() -> Self{
+    println!("Enter Password Entry: ");
+
+    let mut service = String::new();
+
+    io::stdin()
+      .read_line(&mut service)
+      .expect("Failed to read line.");
+
+    println!("Enter Username: ");
+
+    let mut username = String::new();
+
+    io::stdin()
+      .read_line(&mut username)
+      .expect("Failed to read line.");
+
+      println!("Enter Password: ");
+
+      let mut password = String::new();
+
+      io::stdin()
+        .read_line(&mut password)
+        .expect("Failed to read line.");
+  }
 }
